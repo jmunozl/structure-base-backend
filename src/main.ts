5549005ece38d387c0053100d8984edf09489088
Jose Munoz <jos.munozl@gmail.com>
2022-03-29 23:33:44 -0300

    Swagger Openapi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	const config = new DocumentBuilder()
+		.setTitle('Structure Backend')
+		.setDescription('The structure backend API description')
+		.setVersion('1.0')
+		.addTag('Login')
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api', app, document)
 	app.enableCors()
 	await app.listen(process.env.PORT || 3000)
 }
