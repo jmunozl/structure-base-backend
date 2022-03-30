e9c48eee0ad43c0d734423f1ce28fc0e308f594e
lynxforce <32674491+lynxforce@users.noreply.github.com>
2022-03-30 17:09:48 -0300

    prueba

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
 	getHello(): object {
-		return { message: 'Hello from API' }
+		return { message: 'Hello from API V2' }
 	}
 }
