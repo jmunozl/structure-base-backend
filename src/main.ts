4c8dcbac909c8a822ad27577375cefe1f56a001e
jmunozl <jos.munozl@gmail.com>
2022-03-30 10:48:46 -0300

    3001

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config)
 	SwaggerModule.setup('api', app, document)
 	app.enableCors()
-	await app.listen(process.env.PORT || 4000)
+	await app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
+		console.log("Server is running.");
+	  });
 }
 bootstrap()
