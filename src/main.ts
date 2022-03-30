e25556320e2ea43fa58988cb917e00c16d32cbff
jmunozl <jos.munozl@gmail.com>
2022-03-30 11:11:25 -0300

    Changes

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,6 @@ async function bootstrap() {
 	const document = SwaggerModule.createDocument(app, config)
 	SwaggerModule.setup('api', app, document)
 	app.enableCors()
-	await app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-		console.log("Server is running.");
-	  });
+	await app.listen(process.env.PORT || 4000)
 }
 bootstrap()
